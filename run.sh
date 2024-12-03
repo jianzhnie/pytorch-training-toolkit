6c8acf2a591d1b44af5adebf7244062376df5ed6
JianZhengNie <jianzhnie@126.com>
2024-12-03 11:17:14 +0800

    Update update torchddp

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
 torchrun  --nnodes=1 --nproc_per_node=2 --rdzv_id=100 --rdzv_backend=c10d torchddp/elastic_ddp.py
+
+torchrun  --nnodes=1 --nproc_per_node=2 --rdzv_id=100 --rdzv_backend=c10d mingpt/main.py
